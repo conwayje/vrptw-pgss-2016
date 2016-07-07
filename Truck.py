7a12fb74eaab2b4e2fd01583becce833772d4647
arushibandi <bandia.gos@gmail.com>
2016-07-06 22:11:13 -0400

    truck.py added method and created file to import customers

--- Truck.py
+++ Truck.py
@@ -1,17 +1,20 @@
 class Truck():
     #class for Truck
-    def __init__(self, x, y, cargo, visited, distance):
+    def __init__(self, number, x, y, cargo, visited, distance):
+        self.number = number #truck id/number
         self.x = x #x location
         self.y = y #y location
         self.cargo = cargo #amount of cargo currently in truck
         self.visited = visited #array of customers previously visited
         self.distance = distance #total distance traveled
         
-    def intersects_path(self): #whether the truck intersects its own path
-        
-        return True
+    def __str__(self):
+        return "<Truck {0}: x = {1}, y = {2}, cargo = {3}, visited = {4}, distance = {5}>".format(self.number, self.x, self.y, self.cargo, self.visited, self.distance)
         
     def distance_to_customer(self, cust): #distance to a given customer
         return ((cust.y - self.y)**2 + (cust.x - self.x)**2)**0.5
         
-        
+    def distance_from_depot(self): #distance from depot
+        return (self.y**2 + self.x**2)**0.5
+    
+    
