f3214cb4938d1d9c9d5e958f274ecb18de60b066
Daniel Qian <daniel.j.qian@gamil.com>
2016-07-07 15:12:59 -0400

    State.py

--- scripts/Cluster.py
+++ scripts/Cluster.py
@@ -1,4 +1,8 @@
+import itertools
+from Path import Path
+
 class Cluster():
+    MAX_LEN_BRUTE_FORCE = 10
 
     def __init__(self, customers):
         self.customers = customers
@@ -14,8 +18,35 @@ class Cluster():
             count += 1
         return (total_x/count, total_y/count)
 
-    def getSolution(self):
-        pass # will return a path, probably brute forced here, as long as len(customers) < a
+    # brute forces a solution if len(customers) <= n, can be overriden
+    def get_solution(self, override):
+        if(len(self.customers) <= self.MAX_LEN_BRUTE_FORCE or override):
+            return None
+
+        min = -1
+        best_path = Path(self.customers)
+        for cs in itertools.permutations(self.customers):
+            p = Path(cs)
+            dist = p.get_distance()
+            if(min == -1 or dist < min):
+                min = dist
+                best_path = p
+
+        return best_path
+
+
+    # old method to get perms, but python already has one
+    # @staticmethod
+    # def generate_perms(customers):
+    #     perms = []
+    #     if len(customers) == 1:
+    #         return customers
+    #
+    #     for c in customers:
+    #         temp = customers
+    #         temp.remove(c)
+    #         perms.append(Cluster.generate_perms(temp))
+    #     return perms
 
     @staticmethod
     def create_clusters(customers):
