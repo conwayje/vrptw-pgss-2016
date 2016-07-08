d3e36ab584ccb0cd6d5bf548416f9c370c7b9d0c
smirchan <Suvir@Suvirs-MBP.wv.cc.cmu.edu>
2016-07-08 11:18:00 -0400

    Added customer numbers to plot

--- scripts/Main.py
+++ scripts/Main.py
@@ -20,9 +20,7 @@ def init():
     truck1 = Truck(1, 0, 0, 700)
     truck2 = Truck(2,0,0,700)
     truck3 = Truck(3,0,0,700)
-    Visual.plot_customers(depot, customers)
-    
-    # Visual.plot_customers(depot, customers)
+    #Visual.plot_customers(depot, customers)
 
 def initial_state():
     global customers, truck1, truck2, truck3
