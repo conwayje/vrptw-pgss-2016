d779e405fee3cf5a1e46ea45fd5ab15c5ee725ac
Daniel Qian <daniel.j.qian@gamil.com>
2016-07-14 15:25:33 -0400

    Path implements Distance matrix

--- scripts/distances.py
+++ scripts/distances.py
@@ -1,9 +1,12 @@
+from Customer import Customer
+
 class Distances():
 
     def __init__(self, customers):
         self.matrix = []
 
         customers = sorted(customers, key = lambda customer: customer.number)
+        customers.insert(Customer(0, 0, 0, 0, 0, 0))
         for c1 in customers:
             row = []
             for c2 in customers:
