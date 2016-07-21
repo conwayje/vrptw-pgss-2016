eb719c6394ad3ede02750be45172d08a04745f05
Daniel Qian <daniel.j.qian@gamil.com>
2016-07-21 18:59:25 -0400

    Prints and plots graphs when interrupted

--- scripts/AStar.py
+++ scripts/AStar.py
@@ -15,82 +15,93 @@ def doAStar(initial_state, world_record = 591.55):
     average_rate = -1000000
     rate = 0
 
-    while ( len(queue) > 0 ) and world_record_not_broken:
-        while(len(queue) > 10000):
-            extra = queue.pop()
-            del extra
 
-        (priority, state) = heappop(queue)
+    try:
+        while ( len(queue) > 0 ) and world_record_not_broken:
+            while(len(queue) > 10000):
+                extra = queue.pop()
+                del extra
 
-        done = False
-        while len(queue) > 0 and not done:
-            (next, next_state) =  queue[0]
-            if next == priority:
-                heappop(queue)
-            else:
-                done = True
+            (priority, state) = heappop(queue)
 
-
-        print "Score of currently explored state: {}".format( priority ),
-        # if(priority < 10000000):
-        print "Distance:", state.calculate_distance(),
-
-        if state.calculate_distance() < world_record:
-            print "Yay"
-            print state
-            print state.get_score()
-
-            # @TODO: truck number dependency
-            # also, this is kind of messy generally...
-            for truck in state.trucks:
-                for c in truck.path.route:
-                    print c.number,
-            queue = []
-            world_record_not_broken = False
-            break
-
-        # print state.get_score(), state
-        children = state.get_children_small_moves()
-        # print len(children)
+            done = False
+            while len(queue) > 0 and not done:
+                (next, next_state) =  queue[0]
+                if next == priority:
+                    heappop(queue)
+                else:
+                    done = True
 
 
-        prev_scores.insert(0, priority)
+            print "Score of currently explored state: {}".format( priority ),
+            # if(priority < 10000000):
+            print "Distance:", state.calculate_distance(),
 
-        if (not len(prev_scores) <= 10):
-            to_remove = prev_scores.pop()
-            diff = priority - to_remove
+            if state.calculate_distance() < world_record:
+                print "Yay"
+                print state
+                print state.get_score()
 
-        if len(prev_scores)>1:
-            rate = priority - prev_scores[1]
+                # @TODO: truck number dependency
+                # also, this is kind of messy generally...
+                for truck in state.trucks:
+                    for c in truck.path.route:
+                        print c.number,
 
-        if counter != 0:
-            average_rate = (rate + average_rate * (counter-1))/counter
+                    print
+                queue = []
+                world_record_not_broken = False
+                break
 
-        counter += 1
-        print average_rate, diff
-
-        if diff > -100000:
-            children = []
+            # print state.get_score(), state
+            children = state.get_children_small_moves()
+            # print len(children)
             for c in state.get_children_big_moves():
                 children.append(c)
             for c in state.get_children_medium_moves():
                 children.append(c)
-            prev_scores = []
-            diff == -1000000
 
 
-        for c in children:
-            heappush(queue, ( score(c), c) )
+            prev_scores.insert(0, priority)
 
-        done = False
-        while len(queue) > 0 and not done:
-            (next, next_state) = queue[0]
-            if next == priority:
-                heappop(queue)
-            else:
-                done = True
+            if (not len(prev_scores) <= 10):
+                to_remove = prev_scores.pop()
+                diff = priority - to_remove
+
+            if len(prev_scores)>1:
+                rate = priority - prev_scores[1]
+
+            if counter != 0:
+                average_rate = (rate + average_rate * (counter-1))/counter
+
+            counter += 1
+            print average_rate, diff
+
+            # if diff > -100000:
+            #     for c in state.get_children_big_moves():
+            #         children.append(c)
+            #     for c in state.get_children_medium_moves():
+            #         children.append(c)
+            #     prev_scores = []
+            #     diff == -100000000
 
 
+            for c in children:
+                heappush(queue, ( score(c), c) )
+
+            done = False
+            while len(queue) > 0 and not done:
+                (next, next_state) = queue[0]
+                if next == priority:
+                    heappop(queue)
+                else:
+                    done = True
+    except:
+        for truck in state.trucks:
+            for c in truck.path.route:
+                print c.number,
+            print
+        state.plot()
     return state
 
 
