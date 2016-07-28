38d0ccea5771ef8941a02c3708fbbf5c90266736
Jeff C <jconway@arcwebtech.com>
2016-07-27 23:38:51 -0400

    Just include proper WR scores

--- scripts/Main.py
+++ scripts/Main.py
@@ -18,8 +18,8 @@ import argparse
 #               RC208.txt, RC208_wr_solution.txt
 
 # sample calls (made from inside the scripts directory):
-# python Main.py C201 C201_wr_solution 3 700 591.55
-# python Main.py RC208 RC208_wr_solution 3 1000 891.65 --plot
+# python Main.py C201 C201_wr_solution 3 700 591.56
+# python Main.py RC208 RC208_wr_solution 3 1000 828.15 --plot
 
 customers = None
 depot = None
