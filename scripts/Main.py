1dfae1b3f75b8594f090b3f3745539ad4f6808a3
smirchan <smirchan@gmail.com>
2016-07-28 10:43:50 -0400

    Fixed import filenames

--- scripts/Main.py
+++ scripts/Main.py
@@ -75,7 +75,7 @@ def initial_state(problem, filename):
         state.plot()
 
     else:
-        state = import_solution(problem + ".txt", filename  + ".txt")
+        state = import_solution(problem, filename )
 
     if do_plot:
         state.plot()
